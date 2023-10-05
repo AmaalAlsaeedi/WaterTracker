8c015558a0fbe57c353113dca473de82d2e179e1
AmaalAlsaeedi <iamamaal@outlook.com>
2023-10-05 12:15:15 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Amaal Ali Alsaeedi on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
